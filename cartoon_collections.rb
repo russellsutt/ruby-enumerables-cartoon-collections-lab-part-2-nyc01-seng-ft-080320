3d0977fc8d80384f62a8a5035b9825667c7d5855
Russell Sutter <russell.sutter@gmail.com>
2020-07-14 20:08:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ def find_valid_calls(planeteer_calls)
   
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  valid_calls.find { |call| call = planeteer_calls.any? }
+  valid_calls.find { |call| planeteer_calls.include?(call) }
   
 end
